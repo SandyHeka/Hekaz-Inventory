eb2159424d3e120841815a78d70466c020c74a7d
sandeshCodes <hekasandyvr46@gmail.com>
2025-06-09 21:59:01 +1000

    minor changes

--- web/src/components/CategoryList.tsx
+++ web/src/components/CategoryList.tsx
@@ -8,7 +8,7 @@ type Props = {
 };
 const CategoryList = ({ categories, onDelete, onEdit }: Props) => {
   return (
-    <table className="min-w-full text-sm text-left bg-white darK:bg-gray-800 rounded-lg shadow">
+    <table className="min-w-full text-sm text-left bg-white dark:bg-gray-800 rounded-lg shadow">
       <thead className="bg-gray-100 dark:bg-gray-800">
         <tr>
           <th className="px-4 py-2 font-medium text-gray-800 dark:text-gray-50">
