c30a3286e9b359669ded5c25b993de9e2d532974
sandeshCodes <hekasandyvr46@gmail.com>
2025-07-20 21:57:20 +1000

    Verificaiton page updated

--- web/src/pages/Verify2FAPage.tsx
+++ web/src/pages/Verify2FAPage.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import { verify2FA } from "../api/authServices";
-
+import invImage from "../assets/inventory.jpg";
 const Verify2FAPage = () => {
   const [token, setToken] = useState("");
   const [error, setError] = useState("");
@@ -31,22 +31,40 @@ const Verify2FAPage = () => {
   };
 
   return (
-    <div className="max-w-md mx-auto mt-10 p-6 border rounded shadow">
-      <h2 className="text-xl font-bold mb-4">Two-Factor Verification</h2>
-      {error && <p className="text-red-500 mb-2">{error}</p>}
-      <input
-        type="text"
-        placeholder="Enter 6-digit code"
-        className="w-full p-2 border rounded mb-4"
-        value={token}
-        onChange={(e) => setToken(e.target.value)}
-      />
-      <button
-        className="w-full bg-blue-600 text-white py-2 rounded"
-        onClick={handleVerify}
-      >
-        Verify
-      </button>
+    <div className="min-h-screen grid grid-cols-1 md:grid-cols-2 dark:bg-gray-700 bg-[#f4f0ec]">
+      {/* Left Side: Form */}
+      <div className="flex items-center justify-center p-8 bg-white shadow">
+        <div className="w-full max-w-md space-y-6">
+          <h2 className="text-xl font-bold mb-4">Two-Factor Verification</h2>
+          {error && <p className="text-red-500 mb-2">{error}</p>}
+          <input
+            type="text"
+            placeholder="Enter 6-digit code"
+            className="w-full p-2 border rounded mb-4"
+            value={token}
+            onChange={(e) => setToken(e.target.value)}
+          />
+          <button className="w-full bg-[#c16e02] text-white py-2 rounded-md hover:bg-[#a5793f] transition">
+            Verify
+          </button>
+        </div>
+      </div>
+      <div className="hidden md:block relative">
+        <img
+          src={invImage}
+          alt="Warehouse"
+          className="absolute inset-0 w-full h-full object-cover"
+        />
+        <div className="absolute inset-0 bg-black/50 flex flex-col justify-center px-12 text-white">
+          <h2 className="text-3xl font-bold mb-2">
+            A simple approach to managing inventory
+          </h2>
+          <p className="text-sm">
+            The simplest inventory software for business and teams to stay on
+            top of their stuff.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
