6f0184d193eb23e0f0e355e6629415600f5c67de
sandeshCodes <hekasandyvr46@gmail.com>
2025-08-02 21:51:59 +1000

    small change

--- web/src/components/ConfirmDialog.tsx
+++ web/src/components/ConfirmDialog.tsx
@@ -16,7 +16,7 @@ const ConfirmDialog = ({ isOpen, onConfirm, onCancel, message }: Props) => {
         <div className="flex justify-end gap-2">
           <button
             onClick={onCancel}
-            className="px-4 py-2 rounded bg-gray-300 hover:bg-gray-400 dark:bg-gray-600 dark:hover:bg-gray-700"
+            className="px-4 py-2 rounded bg-green-700 dark:bg-gray-600 text-white  "
           >
             Cancel
           </button>
